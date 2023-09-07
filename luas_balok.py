99e714520aed00342c2275958222146b6975e989
wira78 <144327927+wira78@users.noreply.github.com>
2023-09-07 16:04:06 +0700

    Add files via upload

---
+++ luas_balok.py
@@ -0,0 +1,9 @@
+# menghitung luas balok
+print ("program menghitung volume")
+
+p = int(input("masukan panjang balok: "))
+l = int(input("masukan lebar balok: "))
+t = int(input("masukan tinggi balok: "))
+volume = p*l*t
+
+print ("jadi volume balok adalah =", volume)
---
+++ volume tabung.py
@@ -0,0 +1,8 @@
+# menghitung volume tabung
+
+pi = 22/7
+jari = float(input("masukan jari-jarinya: "))
+tinggi = float(input("masukan tingginya: "))
+volume = pi*jari*jari*tinggi
+
+print("volume tabung =", volume)
